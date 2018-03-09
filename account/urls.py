f4bcc3272ba671f4819841f824e5654768a3122a
Rahul kumar mandal <officialrahulmandal@gmail.com>
2018-03-09 05:43:06 +0530

    password change view created successfully

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import login, logout, logout_then_login,password_change,password_change_done
 from . import views
 urlpatterns = [
 url(r'^login/$',login,name='login'),
 url(r'^logout/$',logout,name='logout'),
 url(r'^logout-then-login/$',logout_then_login,name='logout_then_login'),
+url(r'^password-change/$',password_change,name="password_change"),
+url(r'^password-change/done/$',password_change_done,name="password_change_done"),
 url(r'^$',views.dashboard,name='dashboard'),
 ]
