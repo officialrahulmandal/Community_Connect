4d1db018b7d8177eabc854f970631ffcc08f93e8
Tushar Malik <itusharmalik62@gmail.com>
2018-03-19 04:08:56 +0530

    Made changes in mails/models.py and mails/admin.py

--- app/mails/admin.py
+++ app/mails/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    Options,
+    Statistics,
+    Logs,
+    Content,
+    ContentMeta,
+    EmailGroups,
+    EmailSchedules,
+    EmailRelations
+)
 
-# Register your models here.
+admin.site.register(Options)
+admin.site.register(Statistics)
+admin.site.register(Logs)
+admin.site.register(Content)
+admin.site.register(ContentMeta)
+admin.site.register(EmailGroups)
+admin.site.register(EmailSchedules)
+admin.site.register(EmailRelations)
+
+
--- app/mails/models.py
+++ app/mails/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Options(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+
+    def __str__(self):
+        return self.key
+
+class Statistics(models.Model):
+    key = models.CharField(max_length=255)
+    counter = models.IntegerField()
+    types = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.key
+
+
+class Logs(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+    types = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.key
+
+
+class Content(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    types = models.CharField(max_length=255)
+    date_added = models.DateField()
+    
+    def __str__(self):
+        return self.title
+
+
+class ContentMeta(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+    content_id = models.ForeignKey('Content', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.key
+
+
+class EmailGroups(models.Model):
+    group_name = models.CharField(max_length=255)
+    label = models.TextField()
+
+    def __str__(self):
+        return self.group_name
+
+
+class EmailSchedules(models.Model):
+    content_id = models.ForeignKey('Content', on_delete=models.CASCADE)
+    email_group = models.ForeignKey('EmailGroups', on_delete=models.CASCADE)
+    schedule_time = models.DateField()
+    status = models.IntegerField()
+
+
+class EmailRelations(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey('EmailGroups', on_delete=models.CASCADE)
