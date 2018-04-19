18d77a85bdb872c6298f2572a806799a28a8acab
Mannu Gupta <abhimanyu98986@gmail.com>
2018-04-19 17:55:35 +0530

    Configure Django Setting :     1. Change directory structure for settings.     2. Make 2 environment i.e development and production

--- app/Community_Connect/wsgi.py
+++ app/Community_Connect/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Community_Connect.settings")
+# Read .env file and set key/value inside it as environement variables
+# see: http://github.com/theskumar/python-dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 
 application = get_wsgi_application()
